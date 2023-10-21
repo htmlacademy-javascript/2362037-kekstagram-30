b10c81e0d902dc13a47b0f497754cc2b56d25bde
екатерина <mono-in-life@yandex.ru>
2023-10-21 15:10:17 +0300

    5.14. Модуляция

---
+++ js/util.js
@@ -0,0 +1,15 @@
+export const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const result = Math.random() * (max - min + 1) + min;
+  return Math.floor(result);
+};
+
+export const createId = () => {
+  let lastId = 0;
+
+  return () => {
+    lastId += 1;
+    return lastId;
+  };
+};
